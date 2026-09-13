5c417d5212ed65eaf595c536692907be15d1ce6d
Athalla Jovian Maharsa <athalla@users.noreply.github.com>
2026-09-13 16:18:58 +0700

    feat: release official Mobile Web PWA, Android APKs, and Apex Sentinel engine

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'skibidi-defense-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './Skibidi_Toilet_Defense_Mobile.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.map((k) => { if (k !== CACHE_NAME) return caches.delete(k); })
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
